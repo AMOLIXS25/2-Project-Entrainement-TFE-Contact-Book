89e00fa22c78848dd9d54b13c004daf13493f5cc
amolixs <amolixs12568@gmail.com>
2022-12-28 19:02:25 +0100

    Build the skeleton of the MainWindow

--- views.py
+++ views.py
@@ -1,5 +1,13 @@
 """This module all the views of the application"""
+from PySide6.QtCore import QSize
+from PySide6.QtWidgets import QMainWindow
 
 
-
-class MainWindow()
+class MainWindow(QMainWindow):
+    """The Main Window of the application"""
+    def __init__(self):
+        """Main Window's constructor"""
+        super().__init__()
+        self.setWindowTitle("Contacts")
+        self.setFixedSize(QSize(600, 300))
+        self.show()
